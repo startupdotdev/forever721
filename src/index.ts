f53780b72fc1ddced9df7d23246826ca505bb6d4
Ryan LaBouve <imryanlabouve@gmail.com>
2022-02-08 16:07:45 -0600

    Work out grade data shape

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,43 @@
-const analyzeMetadata = async (tokenUri: string): Promise<any> => {
-  return await false;
+enum GradeLetter {
+  A = "A",
+  B = "B",
+  C = "C",
+  D = "D",
+  F = "F",
+}
+
+interface Grade {
+  grade: GradeLetter;
+  reasons: Reason[];
+}
+
+enum Severity {
+  Critical = -3,
+  Bad = -1,
+  Notice = 0,
+  Good = 1,
+  Great = 2,
+}
+
+interface Reason {
+  severity: Severity;
+  message: string;
+}
+
+const analyzeMetadata = async (tokenUri: string): Promise<Grade> => {
+  return await {
+    grade: GradeLetter.F,
+    reasons: [
+      {
+        severity: Severity.Critical,
+        message: "EMOTIONAL",
+      },
+      {
+        severity: Severity.Critical,
+        message: "DAMAGE",
+      },
+    ],
+  };
 };
 
 export { analyzeMetadata };
