f90ae02c7f633a648eefa742a15c87d6ed788d14
Ryan LaBouve <imryanlabouve@gmail.com>
2022-02-10 14:21:00 -0600

    Extracts handleBase64 to specific function

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,32 @@ import * as Reasons from "./constants/reasons";
 export const isTokenUriBase64Json = (tokenUri: string): boolean => {
   return tokenUri.startsWith("data:application/json;base64");
 };
+
+export const handleBase64Json = (tokenUri: string): Reason[] => {
+  let reasons: Reason[] = [Reasons.metadataOnChain];
+
+  const encodedData: string = tokenUri.split("data:application/json;base64")[1];
+  const decodedBuffer: Buffer = Buffer.from(encodedData, "base64");
+  const decodedString: string = decodedBuffer.toString();
+  const json: Metadata = JSON.parse(decodedString);
+
+  // todo: what if no image?
+  const imageAttribute = json.image || "";
+
+  if (imageAttribute.startsWith("data:image")) {
+    reasons = [...reasons, Reasons.imageOnChain];
+  }
+
+  return reasons;
+};
+
 const analyzeTokenUri = async (tokenUri: string): Promise<Grade> => {
   let reasons: Reason[] = [];
 
   if (isTokenUriBase64Json(tokenUri)) {
-    reasons = [...reasons, Reasons.metadataOnChain];
-
-    const encodedData: string = tokenUri.split(
-      "data:application/json;base64"
-    )[1];
-    const decodedBuffer: Buffer = Buffer.from(encodedData, "base64");
-    const decodedString: string = decodedBuffer.toString();
-    const json: Metadata = JSON.parse(decodedString);
-
-    // todo: what if no image?
-    const imageAttribute = json.image || "";
-
-    if (imageAttribute.startsWith("data:image")) {
-      reasons = [...reasons, Reasons.imageOnChain];
-    }
+    reasons = handleBase64Json(tokenUri);
   } else {
+    // TODO Make else throw an error
     // Just IPFS atm
     reasons = [Reasons.tokenUriIsIPFS];
   }
