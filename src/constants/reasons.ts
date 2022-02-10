d54bf4e88287a891246b944711f8291b18d63d67
John Mosesman <johnmosesman@gmail.com>
2022-02-10 14:01:05 -0600

    Add id to reason, failing test for IPFS top level

--- src/constants/reasons.ts
+++ src/constants/reasons.ts
@@ -1,13 +1,21 @@
 import { Severity } from "./enums";
 
 const metadataOnChain: Reason = {
+  id: "metadata-on-chain",
   severity: Severity.Great,
   message: "Metadata is on-chain",
 };
 
 const imageOnChain: Reason = {
+  id: "image-on-chain",
   severity: Severity.Great,
   message: "Image is on-chain",
 };
 
-export { imageOnChain, metadataOnChain };
+const tokenUriIsIPFS: Reason = {
+  id: "token-uri-is-ipfs",
+  severity: Severity.Good,
+  message: "TokenURI is hosted on IPFS",
+};
+
+export { imageOnChain, metadataOnChain, tokenUriIsIPFS };
